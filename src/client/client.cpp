9992335e798aa4b2e2c5f385aaafd8ee74757714
fastenough69 <van.lisenkov@yandex.ru>
2025-10-03 00:46:42 +0300

    try to write client

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -3,12 +3,25 @@
 #include <iostream>
 #include <string>
 #include <functional>
+#include <streambuf>
 
 using namespace boost::asio;
 using namespace boost::asio::ip;
 
-int main(void) 
-{
-	io_context con;
-	return 0;
+int main() {
+    io_context service;
+    tcp::resolver resolver(service);
+
+    tcp::resolver::results_type endpoints = resolver.resolve("localhost", "80");
+
+    std::cout << "Resolved addresses:" << std::endl;
+    for (const auto& result : endpoints)
+    {
+        tcp::endpoint ep = result.endpoint();
+        std::cout << ep.address().to_string() << std::endl;
+        auto sock = std::make_shared<tcp::socket>(service);
+        sock->connect(ep);
+        std::cout << "Connected to server\n";
+    }
+    return 0;
 }
