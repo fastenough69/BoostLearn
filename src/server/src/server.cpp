501253aaf5c0d1ff07fca61009ccb9b290e507df
fastenough69 <van.lisenkov@yandex.ru>
2025-10-07 21:12:49 +0300

    refactor server code

---
+++ src/server/src/server.cpp
@@ -0,0 +1,67 @@
+#include "server.hpp"
+
+std::vector<socket_ptr> clients;
+
+BoostServer::BoostServer(std::shared_ptr<io_context> con, const int& port): context_ptr(con), port(port), ep_(ip::address_v4::any(), port), ac_(*(context_ptr.lock()), ep_)
+{
+	sock_ptr = std::make_shared<tcp::socket>(*(context_ptr.lock()));
+}
+
+void BoostServer::socket_accept(func_hand func)
+{
+	try
+	{
+		ac_.async_accept(*(context_ptr.lock()), std::bind(func, sock_ptr, std::ref(ac_), std::placeholders::_1));
+		accepted = true;
+	}
+	catch (std::exception& e)
+	{
+		std::cerr << e.what();
+	}
+}
+
+inline bool BoostServer::is_acepted() const noexcept
+{
+	return accepted;
+}
+
+void BoostServer::server_run() noexcept
+{
+	std::cout << "server is up in port " << port << std::endl;
+	context_ptr.lock()->run();
+}
+
+void handle_accept(socket_ptr sock, tcp::acceptor& ac, const boost::system::error_code& code)
+{
+	if (code) return;
+	clients.push_back(sock);
+	std::cout << "Sock is accepted\n";
+	static const std::string msg("Privet boost!\n");
+	async_write(*sock, buffer(msg),
+		[sock](const boost::system::error_code& ec, std::size_t /*length*/)
+		{
+			if (!ec) {
+				std::cout << "Message sent successfully\n";
+			}
+			else {
+				std::cout << "Send error: " << ec.message() << "\n";
+			}
+		});
+	//sock->close();
+	send_message(sock);
+	auto new_sock = std::make_shared<tcp::socket>(ac.get_executor());
+	ac.async_accept(*new_sock, std::bind(handle_accept, new_sock, std::ref(ac), std::placeholders::_1));
+}
+
+int main(int argc, char** argv)
+{
+	auto con_ptr = std::make_shared<io_context>();
+	BoostServer serv(con_ptr, 2020);
+	func_hand func = handle_accept;
+	serv.socket_accept(func);
+	if (serv.is_acepted())
+	{
+		serv.server_run();
+	}
+	return 0;
+}
---
+++ src/server/src/server.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <boost/asio.hpp>
+#include <memory>
+#include <iostream>
+#include <string>
+#include <functional>
+#include <set>
+
+using namespace boost::asio;
+using namespace ip;
+using socket_ptr = std::shared_ptr<tcp::socket>;
+using func_hand = void(*)(socket_ptr sock, tcp::acceptor& ac, const boost::system::error_code& code);
+
+void handle_accept(socket_ptr sock, tcp::acceptor& ac, const boost::system::error_code& code);
+void send_message(socket_ptr sock);
+
+class BoostServer {
+	std::weak_ptr<io_context> context_ptr;
+	int port;
+	tcp::endpoint ep_;
+	tcp::acceptor ac_;
+	socket_ptr sock_ptr = nullptr;
+	bool accepted = false;
+	std::set<tcp::socket> clients;
+
+	void swap(BoostServer copy) noexcept;
+
+public:
+	BoostServer() = default;
+	BoostServer(std::shared_ptr<io_context> con, const int& port);
+	BoostServer(const BoostServer& right) = delete;
+	BoostServer& operator=(const BoostServer& right) = delete;
+	BoostServer(BoostServer&& right) noexcept;
+	BoostServer& operator=(BoostServer&& right) noexcept;
+	void socket_accept(func_hand func);
+	inline bool is_acepted() const noexcept;
+	void server_run() noexcept;
+	
+};
